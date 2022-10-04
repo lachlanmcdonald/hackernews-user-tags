e69cb4d99b4a53245f4e04c348b1370e106baea6
Lachlan McDonald <lachlan@radelaide.net>
2022-10-04 21:40:26 +0100

    RF

--- src/userscript.ts
+++ src/userscript.ts
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         HackerNews Tag User
-// @version      0.1
+// @version      0.2
 // @description  Add custom tags/flair to a user on HackerNews
 // @author       Lachlan McDonald <https://twitter.com/lachlanmcdonald>
 // @match        https://news.ycombinator.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=news.ycombinator.com
+// @updateURL    https://github.com/lachlanmcdonald/hackernews-user-tags/blob/dev/dist/userscript.js
 // @grant        GM.getValue
 // @grant        GM.setValue
 // @run-at       document-idle
