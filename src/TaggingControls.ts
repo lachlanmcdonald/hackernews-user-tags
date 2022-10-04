4f9a0d1f45d0984796bb8110c10538b4c40454ee
Lachlan McDonald <lachlan@radelaide.net>
2022-10-04 20:49:59 +0100

    WIP

--- src/TaggingControls.ts
+++ src/TaggingControls.ts
@@ -65,6 +65,34 @@ export default class TaggingControls {
 		this.setup();
 	}
 
+	static rgbToHex(r: number, g: number, b:number) {
+		const k = [r, g, b].map(x => x.toString(16).padStart(2, '0'));
+		return `#${k.join()}`;
+	}
+
+	static hexToRgb(hex: string) {
+		const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+		hex = hex.replace(shorthandRegex, function(_m: string, r: string, g: string, b:string) {
+			return [r, r, g, g, b, b].join('');
+		});
+
+		const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+		return result ? {
+			r: parseInt(result[1], 16),
+			g: parseInt(result[2], 16),
+			b: parseInt(result[3], 16),
+		} : {
+			r: 0,
+			g: 0,
+			b: 0,
+		};
+	}
+
+	static luminance(hex: string) {
+		const { r, g, b} = TaggingControls.hexToRgb(hex);
+		return 0.2126 * (r / 255) + 0.7152 * (g / 255) + 0.0722 * (b / 255);
+	}
+
 	load() {
 		return GM.getValue(TaggingControls.GM_KEY, '{}').then(data => {
 			this.tags = new Map(Object.entries(JSON.parse(data)));
@@ -111,8 +139,12 @@ export default class TaggingControls {
 
 				if (typeof label === 'string' && label.length) {
 					links.forEach(e => {
+						const backgroundColor = color || TaggingControls.DEFAULT_BACKGROUND;
+						const textColor = TaggingControls.luminance(backgroundColor) > 0.5 ? '#000' : '#FFF';
+
 						e.dataset.tag = label;
-						e.style.setProperty('--bg', color || TaggingControls.DEFAULT_BACKGROUND);
+						e.style.setProperty('--bg', backgroundColor);
+						e.style.setProperty('--color', textColor);
 						e.classList.add(TaggingControls.CSS_CLASS);
 					});
 				} else {
@@ -135,22 +167,34 @@ export default class TaggingControls {
 		controlNode.classList.add(TaggingControls.CSS_CONTROL_CLASS);
 		controlNode.setAttribute('aria-hidden', 'true');
 
-		profileNode.textContent = 'View profile';
+		profileNode.setAttribute('title', 'View Profile');
+		profileNode.classList.add('profile');
+		profileNode.classList.add('button');
 
 		colorInputNode.setAttribute('type', 'color');
 
 		tagInputNode.setAttribute('type', 'text');
+		tagInputNode.setAttribute('placeholder', 'Tag');
 		tagInputNode.setAttribute('maxlength', '16');
 
 		saveButton.setAttribute('type', 'button');
+		saveButton.setAttribute('title', 'Save');
 		saveButton.classList.add('save');
+		saveButton.classList.add('button');
 
 		closeButton.setAttribute('type', 'button');
+		closeButton.setAttribute('title', 'Close');
 		closeButton.classList.add('close');
+		closeButton.classList.add('button');
+
+		const tagInputNodeContainer = document.createElement('div');
+		tagInputNodeContainer.appendChild(tagInputNode);
+		const colorInputNodeContainer = document.createElement('div');
+		colorInputNodeContainer.appendChild(colorInputNode);
 
 		controlNode.appendChild(profileNode);
-		controlNode.appendChild(tagInputNode);
-		controlNode.appendChild(colorInputNode);
+		controlNode.appendChild(tagInputNodeContainer);
+		controlNode.appendChild(colorInputNodeContainer);
 		controlNode.appendChild(saveButton);
 		controlNode.appendChild(closeButton);
 
@@ -172,28 +216,32 @@ export default class TaggingControls {
 			padding: 1px 4px;
 			border-radius: 4px;
 			background: var(--bg);
-			color: #000;
+			color: var(--color);
 			margin: 0 0.25rem;
 			vertical-align: baseline;
 			font-size: 7pt; }`;
 		styleNode.textContent += `.${TaggingControls.CSS_CONTROL_CLASS} {
 			z-index: 1000;
-			top: var(--top);
-			left: var(--left);
-			position: absolute;
-			background: #000;
-			padding: 2px 8px 2px 8px;
-			border-radius: 4px;
-			box-shadow: 2px 2px #00000038;
-			font-size: 7pt;
-			display: flex;
-			flex-flow: row nowrap;
-			gap: 8px;
-			align-items: center;
+top: var(--top);
+left: var(--left);
+position: absolute;
+background: #FFF;
+border-radius: 6px;
+box-shadow: 0px 2px 3px #00000038;
+font-size: 9pt;
+display: flex;
+flex-flow: row nowrap;
+align-items: stretch;
+overflow: hidden;
+border: solid 1px #00000038;
+align-content: center;
 		}`;
 		styleNode.textContent += `.${TaggingControls.CSS_CONTROL_CLASS} > a {
-			color: #FFF;
-			text-decoration: none;
+			color: #000;
+text-decoration: none;
+			display: flex;
+padding: 2px 8px;
+align-items: center;
 		}`;
 		styleNode.textContent += `.${TaggingControls.CSS_CONTROL_CLASS} > a:hover {
 			text-decoration: underline;
@@ -201,50 +249,59 @@ export default class TaggingControls {
 		styleNode.textContent += `.${TaggingControls.CSS_CONTROL_CLASS}[aria-hidden=true] {
 			display: none;
 		}`;
-		styleNode.textContent += `.${TaggingControls.CSS_CONTROL_CLASS} > input[type="text"] {
-			background: #666;
-			color: #FFF;
-			font-family: sans-serif;
-			font-size: 8pt;
-			padding: 4px;
-			line-height: 1;
-			border: 0;
-			appearance: none;
-			text-shadow: none;
-			display: block;
-			width: 170px;
-			border-radius: 3px;
-			outline: 0;
+		styleNode.textContent += `.${TaggingControls.CSS_CONTROL_CLASS} input[type="text"] {
+			background: #f2f2f2;
+color: #000;
+font-family: inherit;
+font-size: 9pt;
+appearance: none;
+width: 170px;
+outline: 0;
+border-left: solid 1px #00000038;
+border-right: solid 1px #00000038;
+border-top: 0;
+border-bottom: 0;
+padding: 0 10px;
 		}`;
-		styleNode.textContent += `.${TaggingControls.CSS_CONTROL_CLASS} > input[type="color"] {
-			width: 30px;
+		styleNode.textContent += `.${TaggingControls.CSS_CONTROL_CLASS} input[type="color"] {
+			width: 38px;
 			outline: 0;
 			border: 0;
 			background: none;
 		}`;
-		styleNode.textContent += `.${TaggingControls.CSS_CONTROL_CLASS} > button {
+		styleNode.textContent += `.${TaggingControls.CSS_CONTROL_CLASS} > div {
+			display: flex;
+			align-items: center;
+		}`;
+		styleNode.textContent += `.${TaggingControls.CSS_CONTROL_CLASS} > .button {
 			display: inline-block;
 			cursor: pointer;
-			width: 20px;
-			height: 20px;
+			width: 38px;
+			height: 38px;
 			border: 0;
+			padding: 0;
 			appearance: none;
 			background: transparent;
-			background-size: 14px auto;
 			background-repeat: no-repeat;
 			background-position: center;
-			opacity: 0.5;
+			opacity: 0.6;
 		}`;
-		styleNode.textContent += `.${TaggingControls.CSS_CONTROL_CLASS} > button:hover {
+		styleNode.textContent += `.${TaggingControls.CSS_CONTROL_CLASS} > .button:hover {
 			opacity: 1.0;
 		}`;
-		styleNode.textContent += `.${TaggingControls.CSS_CONTROL_CLASS} > button.save {
-			background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='321.52 316.78 108.97 118.42'%3E%3Cpath d='M334.36 378.89a7.106 7.106 0 0 0-9.934-1.504 7.102 7.102 0 0 0-1.503 9.934l33.148 44.988c3.019 4.097 9.246 3.785 11.836-.598l61.566-104.19a7.1 7.1 0 0 0-2.504-9.727 7.103 7.103 0 0 0-9.73 2.5l-56.103 94.941z' fill='%23FFF'%3E%3C/path%3E%3C/svg%3E");
+		styleNode.textContent += `.${TaggingControls.CSS_CONTROL_CLASS} > .button.profile {
+			background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='176.8 162.88 398.4 426.23'%3E%3Cpath d='M479.4 266.29c0 57.105-46.293 103.4-103.4 103.4-57.102 0-103.39-46.293-103.39-103.4 0-57.102 46.293-103.39 103.39-103.39 57.105 0 103.4 46.293 103.4 103.39'/%3E%3Cpath d='M176.83 537.26c0 69.137 398.34 69.137 398.34 0 0-92.551-89.172-167.58-199.17-167.58s-199.17 75.027-199.17 167.58z' fill='%23000' /%3E%3C/svg%3E%0A");
+			background-size: 14px auto;
 		}`;
-		styleNode.textContent += `.${TaggingControls.CSS_CONTROL_CLASS} > button.close {
-			background-image: url("data:image/svg+xml,%3Csvg viewBox='256.17 233.43 261.45 261.45' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='m406.79 364.16 106.08-106.55c5.684-5.684 5.684-14.68 0-19.891-5.684-5.684-14.68-5.684-19.891 0l-106.08 106.56-106.55-106.56c-5.684-5.684-14.68-5.684-19.891 0-5.684 5.684-5.684 14.68 0 19.891l106.08 106.55-106.08 106.56c-5.684 5.684-5.684 14.68 0 19.891 2.84 2.84 6.629 4.262 9.945 4.262 3.317 0 7.106-1.422 9.946-4.262l106.55-106.55 106.55 106.55c2.84 2.84 6.628 4.262 9.945 4.262 3.316 0 7.105-1.422 9.945-4.262 5.684-5.684 5.684-14.68 0-19.891z' fill='%23FFF'/%3E%3C/svg%3E");
+		styleNode.textContent += `.${TaggingControls.CSS_CONTROL_CLASS} > .button.save {
+			background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='195.55 251.68 360.62 248.65'%3E%3Cpath d='M523.8 257.23 325.89 455.14l-98.27-89.98a18.94 18.94 0 0 0-13.723-5.238 18.941 18.941 0 0 0-11.852 33.18l111.62 102.25a18.941 18.941 0 0 0 26.187-.567l210.74-210.74v-.004a18.95 18.95 0 0 0 4.907-18.309 18.95 18.95 0 0 0-13.402-13.402 18.962 18.962 0 0 0-18.31 4.906z' fill='%23000' /%3E%3C/svg%3E%0A");
+			background-size: 16px auto;
 		}`;
-		styleNode.textContent += `.${TaggingControls.CSS_CONTROL_CLASS} > button:disabled {
+		styleNode.textContent += `.${TaggingControls.CSS_CONTROL_CLASS} > .button.close {
+			background-image: url("data:image/svg+xml,%3Csvg viewBox='256.17 233.43 261.45 261.45' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='m406.79 364.16 106.08-106.55c5.684-5.684 5.684-14.68 0-19.891-5.684-5.684-14.68-5.684-19.891 0l-106.08 106.56-106.55-106.56c-5.684-5.684-14.68-5.684-19.891 0-5.684 5.684-5.684 14.68 0 19.891l106.08 106.55-106.08 106.56c-5.684 5.684-5.684 14.68 0 19.891 2.84 2.84 6.629 4.262 9.945 4.262 3.317 0 7.106-1.422 9.946-4.262l106.55-106.55 106.55 106.55c2.84 2.84 6.628 4.262 9.945 4.262 3.316 0 7.105-1.422 9.945-4.262 5.684-5.684 5.684-14.68 0-19.891z' fill='%23000'/%3E%3C/svg%3E");
+			background-size: 14px auto;
+		}`;
+		styleNode.textContent += `.${TaggingControls.CSS_CONTROL_CLASS} > .button:disabled {
 			opacity: 0.2;
 		}`;
 
@@ -276,7 +333,7 @@ export default class TaggingControls {
 
 	showControls(target: HTMLElement) {
 		const { left, top, height } = target.getBoundingClientRect();
-		const topRounded = (top + height + 8).toFixed(0);
+		const topRounded = (top + height).toFixed(0);
 		const leftRounded = left.toFixed(0);
 		this.elements.containers.controls.style.setProperty('--top', `${topRounded}px`);
 		this.elements.containers.controls.style.setProperty('--left', `${leftRounded}px`);
